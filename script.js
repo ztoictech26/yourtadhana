3218f0017cbb56d1bf698e4c330937273caebfe9
Tristan Jay Palabrica <tristanjaypalabricaofficial@gmail.com>
2026-02-04 09:09:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function goToProfile() {
+    const input = document.getElementById("nameInput").value.trim().toLowerCase();
+    const message = document.getElementById("message");
+
+    const profileMap = {
+        "john lester": "https://www.facebook.com/media/set/?set=a.105051247626905&type=3",
+        "john wayne": "https://www.facebook.com/altheamillandiane.anilao.7",
+        "althea": "https://www.facebook.com/johnwayne.bigsat",
+        "ismar": "https://www.facebook.com/yskajhdee",
+        "myka": "https://www.facebook.com/ismar.genodia.2024",
+        "kenneth": "https://www.facebook.com/danica.castino.50",
+        "troy": "https://www.facebook.com/tsegura.x"
+    };
+
+    if (profileMap[input]) {
+        window.location.href = profileMap[input];
+    } else {
+        message.textContent = "No Facebook link found for this name.";
+    }
+}
